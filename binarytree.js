8d710d162f850c719cc5688f9a30358c04f40aa3
Khondakar Readul Islam <khondakar22@users.noreply.github.com>
2021-09-21 23:04:49 +0200

    Add traverser of BF and DF

--- binarytree.js
+++ binarytree.js
@@ -15,6 +15,58 @@ class Node {
   }
 }
 
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  traverseBF(fn) {
+    let result = [];
+    let arr = [this.root]; 
+    while(arr.length) {
+      const node = arr.shift();
+     // for(let child of node.children) {
+       // arr.push(child);
+      //}
+      arr.push(...node.children);
+      fn(node);
+    }
+   
+  }
+  
+   traverseDF(fn) {
+    let result = [];
+    let arr = [this.root]; 
+    while(arr.length) {
+      const node = arr.shift();
+     // for(let child of node.children) {
+       // arr.push(child);
+      //}
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  
+  }
+}
+
 const treeNode = new Node(8);
-treeNode.add(10)
-console.log(treeNode.data); // 100
+treeNode.add(9);
+treeNode.add(10);
+treeNode.add(11);
+const treeNode1 = new Node(12);
+
+treeNode1.add(13);
+treeNode1.add(14);
+treeNode1.add(15);
+
+const tree = new Tree();
+tree.root = treeNode;
+
+const tree1 = new Tree();
+tree1.root = treeNode1;
+
+tree1.traverseBF((node) => {
+  console.log(node.data)
+} );
+tree.traverseDF((node) => {
+  console.log(node.data)
+} );
