86b3e3dc09dbca790f38ab8e380c793192a54bda
Khondakar Readul Islam <khondakar22@users.noreply.github.com>
2021-10-05 15:11:16 +0200

    ducktype explanation added

--- uniontype.ts
+++ uniontype.ts
@@ -12,3 +12,35 @@ type SizeIcon = `${Icon} - ${Size}`;
 
 
 let myIcon: SizeIcon = 'a - 20'
+
+
+
+// Duck Typing or Structural Typing
+// Type Checking focuses on the shape that values have. 
+
+// If two objects have the same shape, they are considered to be of the same type.
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+
+const point = {x:12, y:26};
+logPoint(point);
+
+// Point variable is never delecared a type of Point, but typescript compare that it is the same shape so it passes. 
+
+const point3 = { x: 12, y: 26, z: 89 };
+logPoint(point3); // logs "12, 26"
+ 
+const rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect); // logs "33, 3"
+ 
+const color = { hex: "#187ABF" };
+logPoint(color);
