c329b16106e70cc2b48cfeeafa2001b2a888bd35
Khondakar Readul Islam <khondakar22@users.noreply.github.com>
2021-12-07 15:27:25 +0100

    Create Orderby Generic CS

---
+++ orderbygeneric.cs
@@ -0,0 +1,38 @@
+    public static class IQueryableExtension
+    {
+        public static bool IsOrdered<T>(this IQueryable<T> queryable)
+        {
+            if (queryable == null)
+            {
+                throw new ArgumentNullException("queryable");
+            }
+
+            return queryable.Expression.Type == typeof(IOrderedQueryable<T>);
+        }
+
+        public static IQueryable<T> SmartOrderBy<T, TKey>(this IQueryable<T> queryable, Expression<Func<T, TKey>> keySelector)
+        {
+            if (queryable.IsOrdered())
+            {
+                var orderedQuery = queryable as IOrderedQueryable<T>;
+                return orderedQuery.ThenBy(keySelector);
+            }
+            else
+            {
+                return queryable.OrderBy(keySelector);
+            }
+        }
+
+        public static IQueryable<T> SmartOrderByDescending<T, TKey>(this IQueryable<T> queryable, Expression<Func<T, TKey>> keySelector)
+        {
+            if (queryable.IsOrdered())
+            {
+                var orderedQuery = queryable as IOrderedQueryable<T>;
+                return orderedQuery.ThenByDescending(keySelector);
+            }
+            else
+            {
+                return queryable.OrderByDescending(keySelector);
+            }
+        }
+    }
